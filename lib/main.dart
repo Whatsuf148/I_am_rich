a4deb4fffb80c29f54b51a7a687c77fd22a060b6
Whatsuf148 <whatsufsachin@gmail.com>
2022-04-11 22:24:29 +0545

    Added Asset Image

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,50 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.blueGrey,
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text(
-            'I am rich',
-            textAlign: TextAlign.center,
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: true,
+        home: Scaffold(
+          drawer: Drawer(
+              child: Column(
+            children: [
+              SizedBox(height: 100),
+              InkWell(
+                onTap: () => print("Tapped"),
+                child: Container(
+                  alignment: Alignment.center,
+                  color: Colors.red,
+                  height: 100,
+                  width: double.infinity,
+                  child: Text(
+                    "Sachin ",
+                    style: TextStyle(color: Colors.blue),
+                  ),
+                ),
+              ),
+              Text(
+                "Sachin ",
+                style: TextStyle(color: Colors.blue),
+              ),
+            ],
+          )),
+          backgroundColor: Colors.blueGrey,
+          appBar: AppBar(
+            centerTitle: true,
+            title: Text(
+              'I am rich',
+              textAlign: TextAlign.center,
+            ),
+            backgroundColor: Colors.teal,
           ),
-          backgroundColor: Colors.grey[900],
-        ),
-        body: Center(
-          child: Image(
-              image: NetworkImage(
-                  'https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmlld3xlbnwwfHwwfHw%3D&w=1000&q=80')),
-        ),
-      )));
+          body: Center(child: Image.asset("images/diamond.png")),
+        ));
+  }
 }
